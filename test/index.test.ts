d235201af962fcfe5d89a5d3ffcb759fb0818b2b
Sylvain Goumy <goumsss@protonmail.com>
2023-03-22 14:18:28 +0100

    test: fix HelloWorld test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,31 +1,24 @@
 import hre from "hardhat";
 import { expect } from "chai";
-import {} from "../../typechain";
 import { before } from "mocha";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { time } from "@nomicfoundation/hardhat-network-helpers";
-import {
-  Web3FunctionResultCanExec,
-  Web3FunctionResultNotExec,
-  Web3FunctionUserArgs,
-} from "@gelatonetwork/web3-functions-sdk";
-const { ethers, deployments, w3f } = hre;
+import { Web3FunctionUserArgs } from "@gelatonetwork/web3-functions-sdk";
+const { deployments, w3f } = hre;
 
-describe("CoingeckoOracle Tests", function () {
+describe("HelloWorld Tests", function () {
   this.timeout(0);
 
-  let owner: SignerWithAddress;
-
-  let oracle: CoingeckoOracle;
   let userArgs: Web3FunctionUserArgs;
   let storage: { [key: string]: string };
 
   before(async function () {
     await deployments.fixture();
 
-    [owner] = await hre.ethers.getSigners();
-
     userArgs = {};
     storage = {};
+  });
+
+  it("Return canExec: true", async () => {
+    const { result } = await w3f.run("helloWorld", storage, userArgs);
+    expect(result.canExec).to.equal(true);
   });
 });
