2b62acfab317be534d6f5e586ce44168105e7b38
Brandon Chuah <songlin.chuah@gmail.com>
2023-05-04 15:17:44 +0800

    fix: list-secret script

--- scripts/list-secrets.ts
+++ scripts/list-secrets.ts
@@ -1,23 +1,17 @@
 import hre from "hardhat";
 import { Web3Function } from "@gelatonetwork/automate-sdk";
-import { Web3FunctionLoader } from "@gelatonetwork/web3-functions-sdk/loader";
 
 const { ethers } = hre;
 
 const main = async () => {
-  const name = process.argv[2];
-  if (!name) throw new Error("Missing positional argument 'w3f name'");
+  const taskId = process.argv[2];
 
   const [deployer] = await ethers.getSigners();
   const chainId = (await ethers.provider.getNetwork()).chainId;
   const web3Function = new Web3Function(chainId, deployer);
 
-  const { secrets } = Web3FunctionLoader.load(name, hre.config.w3f.rootDir);
-
-  await web3Function.secrets.set(secrets);
-
   // // Get list of secrets
-  const secretsList = await web3Function.secrets.list();
+  const secretsList = await web3Function.secrets.list(taskId);
   console.log(`Secrets list: `);
   console.dir(secretsList);
 };
