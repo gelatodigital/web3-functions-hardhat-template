501519c812683dc1ed2624be5249ce8917b5c544
Brandon Chuah <songlin.chuah@gmail.com>
2023-07-27 13:03:26 +0700

    chore: add linea configs & deployments

--- deploy/AdvertisingBoard.deploy.ts
+++ deploy/AdvertisingBoard.deploy.ts
@@ -14,6 +14,8 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const opsProxyFactory =
     hre.network.name === "zksync"
       ? "0xB675ce934431e42Fd3daE03e3a385b7e24ae7257"
+      : hre.network.name === "linea"
+      ? "0x44bde1bccdD06119262f1fE441FBe7341EaaC185"
       : hre.network.name === "avalanche"
       ? "0x2807B4aE232b624023f87d0e237A3B1bf200Fd99"
       : "0x44bde1bccdD06119262f1fE441FBe7341EaaC185";
