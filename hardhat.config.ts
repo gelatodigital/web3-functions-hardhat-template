bd1b4e514121d07f430fcf3067a3df3af8c98954
Brandon Chuah <songlin.chuah@gmail.com>
2023-08-21 15:33:41 +0700

    chore: add base & polygonzk configs & deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -62,9 +62,9 @@ const config: HardhatUserConfig = {
       url: "https://arb1.arbitrum.io/rpc",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
-    baseGoerli: {
-      chainId: 84531,
-      url: "https://goerli.base.org",
+    base: {
+      url: `https://mainnet.base.org`,
+      chainId: 8453,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
     bsc: {
@@ -97,6 +97,11 @@ const config: HardhatUserConfig = {
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
+    polygonzk: {
+      url: "https://zkevm-rpc.com",
+      chainId: 1101,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
     zksync: {
       zksync: true,
       url: "https://mainnet.era.zksync.io",
@@ -110,6 +115,11 @@ const config: HardhatUserConfig = {
     arbgoerli: {
       chainId: 421613,
       url: `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_ID}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    baseGoerli: {
+      chainId: 84531,
+      url: "https://goerli.base.org",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
     goerli: {
