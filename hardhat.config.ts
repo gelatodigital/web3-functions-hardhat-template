f077ff6078d62b65ba4ff32ff037817f79660070
brandon.cs <songlin.chuah@gmail.com>
2023-11-09 12:07:56 +0800

    chore: add zkatana, sepolia, unreal, geloptestnet configs (#6)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -127,6 +127,11 @@ const config: HardhatUserConfig = {
       url: "https://goerli.base.org",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
+    geloptestnet: {
+      chainId: 42069,
+      url: `https://rpc.op-testnet.gelato.digital`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
     goerli: {
       chainId: 5,
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_ID}`,
@@ -135,6 +140,21 @@ const config: HardhatUserConfig = {
     mumbai: {
       chainId: 80001,
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_ID}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_ID}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    unreal: {
+      chainId: 18231,
+      url: `https://rpc.unreal.gelato.digital`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    zkatana: {
+      chainId: 1261120,
+      url: "https://rpc.zkatana.gelato.digital",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
